8120d9e377b50bd74fab6c6ddd8462615620bcbc
Jinay <jinayparekh98@gmail.com>
2018-08-15 18:38:29 +0530

    UI improved and added Company eligibility functionality

--- accounts/views.py
+++ accounts/views.py
@@ -144,7 +144,7 @@ def index(request):
         cgpa = calc(s1, s2, s3, s4, s5, s6, s7, s8)
         obj.cgpa = float(cgpa)
         obj.save()
-    
+
     context = {'UID': UID, 's1': s1,
                 's2': s2, 's3': s3,
                 's4':s4,'s5': s5,'s6':s6,
